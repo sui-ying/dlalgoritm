b50ab74825cc4f4d097eaafd35beba95ed8b95d4
sui-ying <xyc252709151@163.com>
2023-09-12 22:21:28 +0800

    add climb_stairs

--- question/dynamic_programming/src/climb_stairs.cpp
+++ question/dynamic_programming/src/climb_stairs.cpp
@@ -1,6 +1,6 @@
-/* 给定一个共有 $n$ 阶的楼梯，你每步可以上 $1$ 阶或者 $2$ 阶，请问有多少种方案可以爬到楼顶 */
-
 #include "../../utils/common.hpp"
+
+/* 给定一个共有 $n$ 阶的楼梯，你每步可以上 $1$ 阶或者 $2$ 阶，请问有多少种方案可以爬到楼顶 */
 
 /**
  * @brief 动态规划: 从前往后递推
@@ -48,8 +48,34 @@ int climb_stairs_plus(int n){
     return b;
 }
 
+/*
+加以限制:
+给定一个共有 $n$ 阶的楼梯，你每步可以上 $1$ 阶或者 $2$ 阶，**但不能连续两轮跳 $1$ 阶**，请问有多少种方案可以爬到楼顶。
+*/
+int climb_stairs_constraint(int n){
+    if (n==1 or n==2){
+        return 1;
+    }
+
+    // 状态 dp[i][j] 表示处在第 i 阶、并且上一轮跳了 j(1 or 2) 阶
+    vector<vector<int>>dp(n+1, vector<int>(3, 0));
+    dp[1][1] = 1;
+    dp[1][2] = 0;
+    dp[2][1] = 0;
+    dp[2][2] = 1;
+    for (int i=3; i<n+1; i++){
+        dp[i][1] = dp[i - 1][2];
+        dp[i][2] = dp[i - 2][1] + dp[i - 2][2];
+    }
+    
+    return dp[n][1] + dp[n][2];
+}
+
+
 int main(){
     cout << climb_stairs(10) << endl;
     cout << climb_stairs_plus(10) << endl;
+
+    cout << climb_stairs_constraint(10) << endl;
     return 0;
 }
