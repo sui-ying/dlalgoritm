ce2e521f8c891ed6c8297298425f5a3de40e63e7
sui-ying <xyc252709151@users.noreply.github.com>
2023-10-25 10:37:56 +0800

    add utils

---
+++ utils/common.hpp
@@ -0,0 +1,63 @@
+#pragma once
+
+#include<iostream>
+#include<cmath>
+#include<vector>
+#include<algorithm>
+#include<set>
+#include<unordered_map>
+
+#include<numeric>
+
+using namespace std;
+
+/**
+ * 打印一维vector
+ * 如需打印其他C++中数据类型，可以将函数改写成模板函数
+*/
+template<typename T>
+void printVector(vector<T>& arr){
+    for (typename vector<T>::iterator it = arr.begin(); it != arr.end(); it++){
+        cout << *it << " ";
+    }
+    cout << endl;
+}
+
+/**
+ * 打印二维vector
+ * 如需打印其他C++中数据类型，可以将函数改写成模板函数
+*/
+void printVector2d(vector<vector<int>>& arr2d){
+    for (vector<vector<int>>::iterator it = arr2d.begin(); it != arr2d.end(); it++){ // it 为一个指针， 指向二维向量vec的一级迭代器
+        for (vector<int>::iterator jt = it->begin(); jt != it->end(); jt++){
+            cout << *jt << " ";
+        }
+        cout << endl;
+    }
+
+    // for (auto it = arr2d.begin(); it != arr2d.end(); ++it) {
+    //     // it 为一个指针， 指向二维向量vec的一级迭代器
+    //     for (auto jt = it->begin(); jt != it->end(); ++jt) {
+    //         cout << *jt << " ";
+    //     }
+    //     cout << endl;
+    // }
+}
+
+int sumVector(vector<int>& arr){
+    int result = 0;
+    for (vector<int>::iterator it = arr.begin(); it != arr.end(); it++){
+        result += *it;
+    }
+    return result;
+}
+
+/**
+ * 打印unordered_map，输出形式类似python中的字典    
+*/
+void printUnorderedMap(unordered_map<char, int>& um){
+    for (const auto& pair : um) {
+        cout << "{" << pair.first << ": " << pair.second << "} ";
+    }
+    cout << endl;
+}
